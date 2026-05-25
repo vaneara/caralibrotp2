bfb5ad68f807dec793b2f1eab1f7c2d3d0d83061
Tomás Maldocena <tomasmaldocena@MacBook-de-Tomas.local>
2026-05-25 16:56:44 -0300

    feat: Added images folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Bitacora from './pages/Bitacora'
 import Perfiles from './pages/Perfiles'
+import Mensajes from './pages/Mensajes'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/bitacora" element={<Bitacora />} />
       <Route path="/perfiles" element={<Perfiles />} />
+      <Route path="/mensajes" element={<Mensajes />} />
     </Routes>
   )
 }
